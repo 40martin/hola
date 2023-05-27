4e0cf6019db481e85570176331bc7675fc79ff15
Martin Slavsky <martinboca22@yahoo.com.ar>
2023-05-26 21:33:49 -0300

    primer commit

---
+++ desafio-martin-react/src/components/ItemListContainer.jsx
@@ -0,0 +1,17 @@
+const ItemListContainer = ({greeting}) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+
+                    <h1 className="text-center">{greeting}</h1>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ItemListContainer;
